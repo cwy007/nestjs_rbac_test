94c228238e24341be81917c3b6d13ed431abfd05
chanweiyan <chanweiyan007@gmail.com>
2026-05-07 17:17:28 +0800

    fix: npm i --save @nestjs/typeorm typeorm mysql2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { authPlugins } from 'mysql2';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'Cwy17824',
+    database: 'rbac_test',
+    entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+    synchronize: true,
+    logging: true,
+    poolSize: 10,
+    connectorPackage: 'mysql2',
+    extra: {
+      authPlugins: {
+        sha256_password: authPlugins.sha256_password
+      }
+    }
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
